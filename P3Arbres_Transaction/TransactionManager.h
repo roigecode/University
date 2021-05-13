e831223b02c0ab55a33388d5361c66a945fa45d5
roige-dev <roigeee@gmail.com>
2021-05-13 02:20:24 +0200

    Transaction Manager main

--- P3Arbres_Transaction/TransactionManager.h
+++ P3Arbres_Transaction/TransactionManager.h
@@ -25,19 +25,27 @@ public:
     virtual ~TransactionManager();
 
     void loadFromFile(string file_path);
-    void showAll() const;
-    void showAllReverse() const;
+    void showAll(int n) const;
+    void showAllReverse(int n) const;
     void showOldest() const;
     void showNewest() const;
 
     float feesInTotal() const;
     float feesSinceTime(string date) const;
     float feesInTimeInterval(pair<string, string> interval) const;
+    
+    // Mètodes Auxiliars:
+    vector<Transaction> showAll_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const;
+    vector<Transaction> showAllReverse_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const;
+    BinaryTreeNode<string, Transaction>* showOldest_aux(BinaryTreeNode<string, Transaction>* n) const;
+    BinaryTreeNode<string, Transaction>* showNewest_aux(BinaryTreeNode<string, Transaction>* n) const;
+    float feesInTotal_aux(BinaryTreeNode<string, Transaction>* n,float p) const;
+    float feesSinceTime_aux(string date, BinaryTreeNode<string, Transaction>* n,float p) const;
+    float feesInTimeInterval_aux(pair<string,string> interval, BinaryTreeNode<string, Transaction>* n,float p) const;
 
 private:
     float sellingFee;
     float buyingFee;
-    string file_path;
 };
 
 TransactionManager::TransactionManager(float buyingFee, float sellingFee) {
@@ -45,83 +53,245 @@ TransactionManager::TransactionManager(float buyingFee, float sellingFee) {
     this->sellingFee = sellingFee;
 }
 
-TransactionManager::TransactionManager(string file_path, float buyingFee, float sellingFee)
-: TransactionManager(buyingFee, sellingFee) {
-    this->file_path = file_path;
-}
+TransactionManager::TransactionManager(string file_path, float buyingFee, float sellingFee) {}
 
-TransactionManager::~TransactionManager() {
-    BinarySearchTree::~BinarySearchTree();
-}
+TransactionManager::~TransactionManager() {}
 
 void TransactionManager::loadFromFile(string file_path) {
-    this->file_path = file_path;
-    ifstream archiu(this->file_path);
+    fstream arxiu(file_path);
+    string linea, constructor_aux = "", llista_aux[2];
+    int nombre_linea = 0, nombre_parametre = 0;
+    string instant;
+    int id;
+    float quantitat;
+    while (!arxiu.eof()) {
+        getline(arxiu, linea);
+        if (nombre_linea == 0 && linea.compare("") == 0)
+            throw out_of_range("EXCEPCIÓ: Arxiu buit.");
+        if (nombre_linea > 0) {
+            for (int i = 0; i < linea.length(); i++) {
+                if (linea[i] == ',') {
+                    llista_aux[nombre_parametre] = constructor_aux;
+                    constructor_aux = "";
+                    nombre_parametre++;
+                } else {
+                    constructor_aux += linea[i];
+                }
+            }
+            instant = llista_aux[0];
+            id = stoi(llista_aux[1]);
+            quantitat = stof(constructor_aux);
+            nombre_parametre = 0;
+            constructor_aux = "";
+            Transaction t(instant, id, quantitat);
+            this->add(instant, t);
+        }
+        nombre_linea++;
+    }
+    arxiu.close();
 }
 
-void TransactionManager::showAll() const {
-    ifstream archiu(this->file_path);
-    string myText;
-
-    // La primera linia té 26 caràcters, els ignorem fins a trobat una nova linia 
-    archiu.ignore(26, '\n');
-
-    while (getline(archiu, myText))
-        // Output the text from the file
-        cout << myText << endl;
-
-
-    archiu.close();
+void TransactionManager::showAll(int n) const {
+    int n_aux;
+    if (this->isEmpty()) {
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida.");
+    } else {
+        vector<Transaction> transactions;
+        transactions = showAll_aux(this->_root, transactions);
+        for (int i = 0; i < n; i++) {
+            if (i >= transactions.size())
+                throw out_of_range("EXCEPCIÓ: No hi haa tantes transactions.");
+            Transaction t = transactions[i];
+            t.printTransaction();
+            if (i == n - 1) {
+                cout << "Quantes transactions més vols mostrar?";
+                cin >> n_aux;
+                n += n_aux;
+            }
+        }
+    }
 }
 
-void TransactionManager::showAllReverse() const {
-    int lines = 0;
-    string myText;
-    ifstream archiu(this->file_path);
+vector<Transaction> TransactionManager::showAll_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const {
+    if (n->hasLeft()) {
+        v = showAll_aux(n->getLeft(), v);
+    }
+    vector<Transaction> t = n->getValues();
+    for (int i = 0; i < t.size(); i++) {
+        v.push_back(t[i]);
+    }
+    if (n->hasRight()) {
+        v = showAll_aux(n->getRight(), v);
+    }
+    return v;
+}
 
+void TransactionManager::showAllReverse(int n) const {
+    int n_aux;
+    if (this->isEmpty()) {
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida.");
+    } else {
+        vector<Transaction> transactions;
+        transactions = showAllReverse_aux(this->_root, transactions);
+        for (int i = 0; i < n; i++) {
+            if (i >= transactions.size())
+                throw out_of_range("EXCEPCIÓ: No hi han tantes transaccions.");
+            Transaction t = transactions[i];
+            t.printTransaction();
+            if (i == n - 1) {
+                cout << "Quantes transaccions més vols mostrar?";
+                cin >> n_aux;
+                n += n_aux;
+            }
+        }
+    }
+}
 
+vector<Transaction> TransactionManager::showAllReverse_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const {
+    if (n->hasRight()) {
+        v = showAllReverse_aux(n->getRight(), v);
+    }
+    vector<Transaction> t = n->getValues();
+    for (int i = 0; i < t.size(); i++) {
+        v.push_back(t[i]);
+    }
+    if (n->hasLeft()) {
+        v = showAllReverse_aux(n->getLeft(), v);
+    }
+    return v;
 }
 
 void TransactionManager::showOldest() const {
+    if(this->isEmpty()){
+        throw out_of_range ("EXCEPCIÓ: L'estructura està buida");
+    }
+    else{
+        BinaryTreeNode<string, Transaction> *node = showOldest_aux(this->_root);
+        vector<Transaction> transactions = node->getValues();
+        for(int i = 0; i < transactions.size(); i++){
+            Transaction t = transactions[i];
+            t.printTransaction();
+        }
+    }
+}
 
+BinaryTreeNode<string, Transaction>* TransactionManager::showOldest_aux(BinaryTreeNode<string, Transaction>* n) const{
+    if(n->hasLeft()){
+        return showOldest_aux(n->getLeft());
+    }else{
+        return n;
+    }
 }
 
 void TransactionManager::showNewest() const {
+    if(this->isEmpty()){
+        throw out_of_range ("EXCEPCIÓ: L'estructura està buida");
+    }else{
+        BinaryTreeNode<string, Transaction> *node = showNewest_aux(this->_root);
+        vector<Transaction> transactions = node->getValues();
+        for(int i = 0; i < transactions.size(); i++){
+            Transaction t = transactions[i];
+            t.printTransaction();
+        }        
+    }
+}
 
+BinaryTreeNode<string, Transaction>* TransactionManager::showNewest_aux(BinaryTreeNode<string, Transaction>* n) const{
+    if(n->hasRight()){
+        return showNewest_aux(n->getRight());
+    }else{
+        return n;
+    }
 }
 
 float TransactionManager::feesInTotal() const {
-    float total_fees = 0; 
-    ifstream archiu(this->file_path);
-    string preu;
+    if(this->isEmpty())
+        return 0;
+    else
+        return feesInTotal_aux(this->_root, 0);
+}
 
-     archiu.ignore(26, '\n');
-    
-    while (getline(archiu, preu)) {
-        archiu.ignore(16, ',');
-        archiu.ignore(5, ',');
-        
-        cout<<"Preu: "<<preu<<endl;
-        
-        char preuc[1024];
-        strcpy(preuc, preu.c_str());
-        
-        float preuf;
-        preuf = atof(preuc);
-        
-        total_fees += preuf;
+float TransactionManager::feesInTotal_aux(BinaryTreeNode<string, Transaction>* n,float p) const {
+    float preu_t;
+    if(n->hasLeft()){
+        p += feesInTotal_aux(n->getLeft(), 0);
     }
-     
-    cout<<"\n>> Total fees: "<<total_fees<<endl;
-    return total_fees;
+    vector<Transaction> t = n->getValues();
+    for(int i = 0; i < t.size(); i++){
+            preu_t = t[i].getQuantitatTransaccions();
+            if (preu_t > 0)
+                p += preu_t * this->sellingFee;
+            else
+                p -= preu_t * this->buyingFee;
+    }
+    if(n->hasRight()){
+        p += feesInTotal_aux(n->getRight(), 0);
+    }
+    return p;
 }
 
 float TransactionManager::feesSinceTime(string date) const {
-    return 0;
+    if(this->isEmpty())
+        return 0;
+    else
+        return feesSinceTime_aux(date, this->_root, 0);
 }
 
-float TransactionManager::feesInTimeInterval(pair<string, string> interval) const {
-    return 0;
+float TransactionManager::feesSinceTime_aux(string date, BinaryTreeNode<string, Transaction>* n,float p) const {
+    float preu_t;
+    if(n->getKey() >= date){
+        if(n->hasLeft()){
+            p += feesSinceTime_aux(date, n->getLeft(), 0);
+        }
+        vector<Transaction> t = n->getValues();
+        for(int i = 0; i < t.size(); i++){
+                preu_t = t[i].getQuantitatTransaccions();
+                if (preu_t > 0)
+                    p += preu_t * this->sellingFee;
+                else
+                    p -= preu_t * this->buyingFee;
+        }
+    }
+    if(n->hasRight()){
+        p += feesSinceTime_aux(date, n->getRight(), 0);
+    }
+    return p;
+}
+
+float TransactionManager::feesInTimeInterval(pair<string,string> interval) const {
+    if(interval.first > interval.second)
+        throw out_of_range("EXCEPCIÓ: L'interval no té sentit");
+    if(this->isEmpty())
+        return 0;
+    else
+        return feesInTimeInterval_aux(interval, this->_root, 0);
+}
+
+float TransactionManager::feesInTimeInterval_aux(pair<string,string> interval, BinaryTreeNode<string, Transaction>* n,float p) const {
+    float preu_t;
+    if(n->getKey() < interval.first){
+        if(n->hasRight())
+            p += feesInTimeInterval_aux(interval, n->getRight(), 0);
+    }
+    else if(n->getKey() > interval.second){
+        if(n->hasLeft())
+            p += feesInTimeInterval_aux(interval, n->getLeft(), 0);      
+    }
+    else{
+        if(n->hasRight())
+            p += feesInTimeInterval_aux(interval, n->getRight(), 0);
+        if(n->hasLeft())
+            p += feesInTimeInterval_aux(interval, n->getLeft(), 0);        
+        vector<Transaction> t = n->getValues();
+        for(int i = 0; i < t.size(); i++){
+                preu_t = t[i].getQuantitatTransaccions();
+                if (preu_t > 0)
+                    p += preu_t * this->sellingFee;
+                else
+                    p -= preu_t * this->buyingFee;
+        }
+    }
+    return p;
 }
 
 
--- P3Arbres_Transaction/main.cpp
+++ P3Arbres_Transaction/main.cpp
@@ -15,19 +15,58 @@
 
 using namespace std;
 
+void graficarMenu() {
+    const string opcions[10] = {"1. Introduir fitxer", "2. Transaccions ordenades",
+        "3. Transaccions a´l'inrevés", "4. Transaccions del primer instant",
+        "5. Transaccions del darrer instant", "6. Comisió total",
+        "7. Comisió a partir d'una data", "8. Comisió entre dues dates",
+        "9. Balanç de transaccions a queries.txt", "10. Sortir"};
+
+    cout << "----------------------------------------" << endl;
+    for (auto& opcio : opcions)
+        cout << opcio << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "\n >> ";
+}
+
+void tractarOpcio(int opcio, TransactionManager tm) {
+    string arxiu;
+    
+    switch (opcio) {
+        case 1:
+            cout << "Introdueix la ruta del arxiu:";
+            cin>>arxiu;
+            try {
+                tm.loadFromFile(arxiu);
+            } catch (const out_of_range& ex) {
+               cerr<<ex.what()<<endl;;
+            }
+            break;
+            
+        case 2: break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        case 6: break;
+        case 7: break;
+        case 8: break;
+        case 9: break;
+        case 10: break;
+        default: cout << "Opcio incorrecte.\n" << endl;
+            break;
+    }
+}
+
 int main(int argc, char** argv) {
-    
-    string casdeprova1 = "transaction-cas-de-prova.txt";
-    
-    Transaction tr1 = Transaction("2020-01-01 09:23", 11712, 107.78);
-    tr1.printTransaction();
-    
-    cout<<"Obrim manager: "<<endl;
-    TransactionManager tm = TransactionManager();
-    tm.loadFromFile(casdeprova1);
-    tm.showAll();
-    tm.feesInTotal();
-    
+    int opcio;
+    TransactionManager tm;
+
+    do {
+        graficarMenu();
+        cin>>opcio;
+        tractarOpcio(opcio, tm);
+    } while (opcio != 10);
+
     return 0;
 }
 
