b529973dd91ef3d41458f3fbf8efc0bae8146f1f
Alejandro Roigé <49212244+roigecode@users.noreply.github.com>
2021-05-22 20:00:36 +0200

    Delete P3Arbres_Transaction directory

--- P3Arbres_Transaction/build/Debug/Cygwin-Windows/Transaction.o.d
+++
@@ -1,2 +0,0 @@
-build/Debug/Cygwin-Windows/Transaction.o: Transaction.cpp Transaction.h
-Transaction.h:
--- P3Arbres_Transaction/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp BinarySearchTree.h \
- BinaryTreeNode.h Transaction.h TransactionManager.h
-BinarySearchTree.h:
-BinaryTreeNode.h:
-Transaction.h:
-TransactionManager.h:
