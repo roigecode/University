f4e67d52c43f7d1ea4483f2030612ddbe7f35ca0
Alejandro Roigé <49212244+roigecode@users.noreply.github.com>
2021-05-22 20:03:49 +0200

    Broker Transaction Manager - C++ BST

---
+++ Broker_TM_BST_C++/TransactionManager.h
@@ -0,0 +1,317 @@
+
+/* 
+ * File:   TransactionManager.h
+ * Author: Roige
+ *
+ * Created on 12 de mayo de 2021, 0:18
+ */
+
+#ifndef TRANSACTIONMANAGER_H
+#define TRANSACTIONMANAGER_H
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include<cstring>
+#include "Transaction.h"
+#include "BinarySearchTree.h"
+
+using namespace std;
+
+class TransactionManager : protected BinarySearchTree<string, Transaction> {
+public:
+    TransactionManager(float buyingFee = 0.02, float sellingFee = 0.03);
+    TransactionManager(string file_path, float buyingFee = 0.02, float sellingFee = 0.03);
+    virtual ~TransactionManager();
+
+    void loadFromFile(string file_path);
+    void showAll(int n) const;
+    void showAllReverse(int n) const;
+    void showOldest() const;
+    void showNewest() const;
+
+    float feesInTotal() const;
+    float feesSinceTime(string date) const;
+    float feesInTimeInterval(pair<string, string> interval) const;
+
+    // Mètodes Auxiliars:
+    vector<Transaction> showAll_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const;
+    vector<Transaction> showAllReverse_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const;
+    BinaryTreeNode<string, Transaction>* showOldest_aux(BinaryTreeNode<string, Transaction>* n) const;
+    BinaryTreeNode<string, Transaction>* showNewest_aux(BinaryTreeNode<string, Transaction>* n) const;
+    float feesInTotal_aux(BinaryTreeNode<string, Transaction>* n) const;
+    float feesSinceTime_aux(string date, BinaryTreeNode<string, Transaction>* root) const;
+    float feesInTimeInterval_aux(pair<string, string> interval, BinaryTreeNode<string, Transaction>* n) const;
+    float calculaComisio(BinaryTreeNode<string, Transaction>* node) const;
+
+private:
+    float sellingFee;
+    float buyingFee;
+};
+
+TransactionManager::TransactionManager(float buyingFee, float sellingFee) {
+    this->buyingFee = buyingFee;
+    this->sellingFee = sellingFee;
+}
+
+TransactionManager::TransactionManager(string file_path, float buyingFee, float sellingFee) {
+}
+
+TransactionManager::~TransactionManager() {
+}
+
+void TransactionManager::loadFromFile(string file_path) {
+    fstream arxiu(file_path);
+    string linea, constructor_aux = "", llista_aux[2];
+    int nombre_linea = 0, nombre_parametre = 0;
+    string instant;
+    int id;
+    float quantitat;
+
+    while (!arxiu.eof()) {
+        getline(arxiu, linea);
+        if (nombre_linea == 0 && linea.compare("") == 0)
+            throw out_of_range("EXCEPCIÓ: Arxiu buit o amb nom incorrecte.");
+        if (nombre_linea > 0) {
+            for (int i = 0; i < linea.length(); i++) {
+                if (linea[i] == ',') {
+                    llista_aux[nombre_parametre] = constructor_aux;
+                    constructor_aux = "";
+                    nombre_parametre++;
+                } else {
+                    constructor_aux += linea[i];
+                }
+            }
+            instant = llista_aux[0];
+            id = stoi(llista_aux[1]);
+            quantitat = stof(constructor_aux);
+            nombre_parametre = 0;
+            constructor_aux = "";
+            Transaction t(instant, id, quantitat);
+            this->add(instant, t);
+        }
+        nombre_linea++;
+    }
+
+    arxiu.close();
+}
+
+void TransactionManager::showAll(int n) const {
+    int n_aux;
+    vector<Transaction> transactions;
+    transactions = showAll_aux(this->_root, transactions);
+    int imprimides = 0;
+
+    if (this->isEmpty())
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida.");
+    else {
+        // Cas sortir 0: Contemplat perque el for NO iterarà:
+        for (int i = 0; i < n; i++) {
+            if (imprimides >= transactions.size() or n > transactions.size())
+                throw out_of_range("EXCEPCIÓ: No hi ha tantes transactions.");
+            else {
+                Transaction t = transactions[i];
+                t.printTransaction();
+                if (i == n - 1) {
+                    cout << "Quantes transactions més vols mostrar?\n ";
+                    cout << " >> ";
+                    cin >> n_aux;
+                    n += n_aux;
+                }
+                imprimides++;
+            }
+        }
+    }
+
+}
+
+vector<Transaction> TransactionManager::showAll_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const {
+
+    // Investigar porque con n->getLeft()!=nullptr / getRight()!=nullptr da error;
+    // SOLVED: ERROR ERA hasLeft(): 
+    // _left == nullptr; y no _left!=nullptr;
+
+    if (n->getLeft() != nullptr) {
+        v = showAll_aux(n->getLeft(), v);
+    }
+    vector<Transaction> t = n->getValues();
+    for (int i = 0; i < t.size(); i++) {
+        v.push_back(t[i]);
+    }
+    if (n->getRight() != nullptr) {
+        v = showAll_aux(n->getRight(), v);
+    }
+    return v;
+}
+
+void TransactionManager::showAllReverse(int n) const {
+    int n_aux;
+    vector<Transaction> transactions;
+    transactions = showAllReverse_aux(this->_root, transactions);
+    int imprimides = 0;
+
+    if (this->isEmpty())
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida.");
+    else {
+        // Cas sortir 0: Contemplat perque el for NO iterarà:
+        for (int i = 0; i < n; i++) {
+            if (imprimides >= transactions.size() or n > transactions.size())
+                throw out_of_range("EXCEPCIÓ: No hi ha tantes transactions.");
+            else {
+                Transaction t = transactions[i];
+                t.printTransaction();
+                if (i == n - 1) {
+                    cout << "Quantes transactions més vols mostrar?\n ";
+                    cout << " >> ";
+                    cin >> n_aux;
+                    n += n_aux;
+                }
+                imprimides++;
+            }
+        }
+    }
+
+}
+
+vector<Transaction> TransactionManager::showAllReverse_aux(BinaryTreeNode<string, Transaction>* n, vector<Transaction> v) const {
+    // ÍDEM ERROR:
+    // Investigar porque con n->getLeft()!=nullptr / getRight()!=nullptr da error;
+    // SOLVED: ERROR ERA hasLeft(): 
+    // _left == nullptr; y no _left!=nullptr;
+
+    if (n->getRight() != nullptr) {
+        v = showAllReverse_aux(n->getRight(), v);
+    }
+    vector<Transaction> t = n->getValues();
+    for (int i = 0; i < t.size(); i++) {
+        v.push_back(t[i]);
+    }
+    if (n->getLeft() != nullptr) {
+        v = showAllReverse_aux(n->getLeft(), v);
+    }
+    return v;
+}
+
+void TransactionManager::showOldest() const {
+    if (this->isEmpty())
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida");
+    else {
+        BinaryTreeNode<string, Transaction> *node = showOldest_aux(this->_root);
+        vector<Transaction> transactions = node->getValues();
+        Transaction t = transactions[0];
+        t.printTransaction();
+    }
+}
+
+BinaryTreeNode<string, Transaction>* TransactionManager::showOldest_aux(BinaryTreeNode<string, Transaction>* n) const {
+    // Per la propietat de ser BST tenim la més antiga a l'esquerra:
+    if (n->getLeft() != nullptr) {
+        return showOldest_aux(n->getLeft());
+    } else {
+        return n;
+    }
+}
+
+void TransactionManager::showNewest() const {
+    if (this->isEmpty()) {
+        throw out_of_range("EXCEPCIÓ: L'estructura està buida");
+    } else {
+        BinaryTreeNode<string, Transaction> *node = showNewest_aux(this->_root);
+        vector<Transaction> transactions = node->getValues();
+        Transaction t = transactions[0];
+        t.printTransaction();
+
+    }
+}
+
+BinaryTreeNode<string, Transaction>* TransactionManager::showNewest_aux(BinaryTreeNode<string, Transaction>* n) const {
+    // Per la propietat de ser BST tenim la més nova a la dreta:
+    if (n->getRight() != nullptr) {
+        return showNewest_aux(n->getRight());
+    } else {
+        return n;
+    }
+}
+
+float TransactionManager::feesInTotal() const {
+    if (this->isEmpty())
+        return 0;
+    else
+        return feesInTotal_aux(this->_root);
+}
+
+float TransactionManager::feesInTotal_aux(BinaryTreeNode<string, Transaction>* n) const {
+    float preu_t, p = 0;
+    if (n->getLeft() != nullptr) {
+        p += feesInTotal_aux(n->getLeft());
+    }
+    
+    p = calculaComisio(n);
+    
+    if (n->getRight() != nullptr) {
+        p += feesInTotal_aux(n->getRight());
+    }
+    return p;
+}
+
+float TransactionManager::feesSinceTime(string date) const {
+    if (this->isEmpty())
+        return 0;
+    else
+        return feesSinceTime_aux(date, _root);
+}
+
+float TransactionManager::feesSinceTime_aux(string date, BinaryTreeNode<string, Transaction>* node) const {
+    float preu_t, p = 0;
+
+    if (node->getKey() >= date) {
+
+        if (node->hasLeft())
+            p += feesSinceTime_aux(date, node->getLeft());
+
+        p = calculaComisio(node);
+    }
+
+    if (node->getRight() != nullptr)
+        p += feesSinceTime_aux(date, node->getRight());
+
+    return p;
+}
+
+float TransactionManager::feesInTimeInterval(pair<string,string> interval) const {
+
+    if (interval.first == interval.second)
+        return calculaComisio(find(interval.first));
+        
+    if(interval.first > interval.second)
+        throw out_of_range("L'interval no té sentit");
+    if(this->isEmpty())
+        return 0;
+    else
+        return feesSinceTime(interval.first) - feesSinceTime(interval.second);
+}
+
+
+float TransactionManager::calculaComisio(BinaryTreeNode<string, Transaction>* node) const {
+    
+    if (node == nullptr){
+        return 0;
+    }
+
+    //cout << "Entro a calcula comissio" << endl;
+    float comissio = 0;
+
+    for (Transaction t : node->getValues()) {
+        if (t.getQuantitatTransaccions() < 0) {
+            comissio += this->buyingFee * abs(t.getQuantitatTransaccions());
+        } else {
+            comissio += this->sellingFee * abs(t.getQuantitatTransaccions());
+        }
+    }
+    return comissio;
+}
+
+
+#endif /* TRANSACTIONMANAGER_H */
+
+
---
+++ Broker_TM_BST_C++/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,6 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp BinarySearchTree.h \
+ BinaryTreeNode.h TransactionManager.h Transaction.h
+BinarySearchTree.h:
+BinaryTreeNode.h:
+TransactionManager.h:
+Transaction.h:
