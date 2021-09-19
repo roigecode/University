3b36a153014e58b323f1a92001d055fa59d1f9a5
roigecode <roigeee@gmail.com>
2021-09-19 16:42:13 +0200

    Metodes Numerics

---
+++ Practica1/harmonica.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    float i = 1;
+    float sn = 0;
+    float sn1 = 0;
+
+    // Apartat a):
+    do{
+        sn1 = sn;
+        sn += 1/i;
+        i++;
+    }while(sn1 != sn);
+
+    printf("\n Sn := %f || Sn-1 := %f",sn,sn1);
+    printf("\n Valor de i: %f\n",i);
+
+    sn = 0; 
+    sn1 = 0;
+
+    // Apartat b):
+    do{
+        sn1 = sn;
+        sn += 1/i;
+        i--;
+    }while(i > 1);
+
+    printf(" Sn := %f || Sn-1 := %f",sn,sn1);
+}
