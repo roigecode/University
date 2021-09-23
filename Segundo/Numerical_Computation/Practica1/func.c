b16bf0f1cfff6212905e912f20357a667101cd43
roigecode <roigeee@gmail.com>
2021-09-23 11:38:08 +0200

    Advanced Algos added

--- Segundo/Numerical_Computation/Practica1/func.c
+++ Segundo/Numerical_Computation/Practica1/func.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char const *argv[])
 {
+    // Desenvolupament per Taylor de la funció:
+    double x;
+    double funcio = (cos(x)-1)/pow(x,2);
+    double taylorcos = 1-pow(x,2)/2;
+    double taylorfunc = (taylorcos-1)/pow(x,2);
+
+    x = 0.0001;
+
+    double errabs = funcio;
     
+    printf("Error absolut: %f\n",errabs);
     return 0;
 }
--- Segundo/Numerical_Computation/Practica1/succ-float.c
+++ Segundo/Numerical_Computation/Practica1/succ-float.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
     for (int i = 1; i < 41; i++)
     {
         x_n1 = x_n;
-        x_n = exp(-6.2 * pow(x_n,2)) - 0.5;
+        x_n =expf(-6.2 * pow(x_n,2)) - 0.5;
     }
 
     printf("\n Valor de x_40: %f\n",x_n);
